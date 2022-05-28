499b2da87d0856ad96121ef11f6d7fce64db080d
Jess <toxocious@gmail.com>
2022-05-28 15:56:16 -0700

    Implement function to get a database user by their auth code

---
+++ src/util/get-player-by-auth-code.ts
@@ -0,0 +1,15 @@
+import MySQL from '../classes/mysql';
+
+export const getPlayerbyAuthCode = async (authCode: string): Promise<any> => {
+  const PLAYER_DATA = await MySQL.doQuery(
+    'SELECT `ID`, `Username`, `Discord_UserID` FROM `users` WHERE `Auth_Code` = ?',
+    [authCode]
+  ).catch((err) =>
+    console.log(
+      '[Function | getPlayerByAuthCode] Error when performing SQL query',
+      err
+    )
+  );
+
+  return PLAYER_DATA?.[0] ?? undefined;
+};
