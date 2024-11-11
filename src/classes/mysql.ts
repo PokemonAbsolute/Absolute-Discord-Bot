d3abe27c3de57bf863b8e3a1401dce469cc976e3
Jesse Mack <toxocious@gmail.com>
2024-11-11 12:13:54 -0700

    feat: add generic/template return type to MySQL.doQuery

--- src/classes/mysql.ts
+++ src/classes/mysql.ts
@@ -1,58 +1,58 @@
 import * as mysql from 'mysql2/promise';
 
 export default class MySQL {
-  private static _instance: MySQL;
-  private connection: mysql.Connection | undefined;
+    private static _instance: MySQL;
+    private connection: mysql.Connection | undefined;
 
-  public static get instance(): MySQL {
-    return this._instance || (this._instance = new this());
-  }
-
-  public isConnected(): boolean {
-    return this.connection !== null;
-  }
-
-  public static async doQuery(
-    queryString: string,
-    queryParams: any[]
-  ): Promise<any[] | undefined> {
-    try {
-      const [rows, fields]: any = await this._instance.connection?.execute(
-        queryString,
-        queryParams
-      );
-
-      return rows as any[];
-    } catch (error) {
-      console.log(
-        '[Absolute / Discord Bot | MySQL Query] An error occurred while performing a SQL query.',
-        '\n[Query]:\n\t',
-        queryString,
-        '\n[Params]:\n\t',
-        queryParams,
-        '\n[Error]:\n\t',
-        error
-      );
+    public static get instance(): MySQL {
+        return this._instance || (this._instance = new this());
     }
-  }
 
-  public async connectDatabase(): Promise<void> {
-    return mysql
-      .createConnection({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_GAME_DATABASE,
-      })
-      .then((connection): void => {
-        this.connection = connection;
-      })
-      .catch((error): void => {
-        console.error(
-          '[Absolute / Discord Bot] Failed to create MySQL connection for Absolute Chat.',
-          error
-        );
-        process.exit();
-      });
-  }
+    public isConnected(): boolean {
+        return this.connection !== null;
+    }
+
+    public static async doQuery<T>(
+        queryString: string,
+        queryParams: any[]
+    ): Promise<T[] | undefined> {
+        try {
+            const [rows, fields]: any = await this._instance.connection?.execute(
+                queryString,
+                queryParams
+            );
+
+            return rows as T[];
+        } catch (error) {
+            console.log(
+                '[Absolute / Discord Bot | MySQL Query] An error occurred while performing a SQL query.',
+                '\n[Query]:\n\t',
+                queryString,
+                '\n[Params]:\n\t',
+                queryParams,
+                '\n[Error]:\n\t',
+                error
+            );
+        }
+    }
+
+    public async connectDatabase(): Promise<void> {
+        return mysql
+            .createConnection({
+                host: process.env.MYSQL_HOST,
+                user: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_GAME_DATABASE,
+            })
+            .then((connection): void => {
+                this.connection = connection;
+            })
+            .catch((error): void => {
+                console.error(
+                    '[Absolute / Discord Bot] Failed to create MySQL connection for Absolute Chat.',
+                    error
+                );
+                process.exit();
+            });
+    }
 }
